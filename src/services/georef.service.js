ce452286f4f6bbc096eddc982a6afc69513bc936
Demian <aimedemian@gmail.com>
2025-09-22 16:19:22 -0300

    cambio de logica en registro: archivo local para ciudades desde un xls

--- src/services/georef.service.js
+++ src/services/georef.service.js
@@ -1,12 +1,11 @@
 "use strict";
 
-// Ya no necesitamos la lógica de la API externa
-// import { API_URL } from "@/config/constants";
+import { API_URL } from "@/config/constants";
 
 class GeorefClient {
   async getProvincias() {
-    // Apuntamos a nuestra API local, que ahora es súper rápida
-    const res = await fetch(`/api/georef/provincias`);
+    // Apuntamos a nuestra API de backend, que ahora es súper rápida
+    const res = await fetch(`${API_URL}/api/georef/provincias`);
     if (!res.ok) throw new Error("Error obteniendo provincias");
     const data = await res.json();
     return data.data || [];
@@ -18,7 +17,9 @@ class GeorefClient {
     params.set("provincia_id", String(provinciaId));
 
     // Un único endpoint para localidades y comunas
-    const res = await fetch(`/api/georef/localidades?${params.toString()}`);
+    const res = await fetch(
+      `${API_URL}/api/georef/localidades?${params.toString()}`
+    );
     if (!res.ok) throw new Error("Error obteniendo localidades/comunas");
     const data = await res.json();
     return data.data || [];
