efb6beefbe7df1c7b0c6b3b107305551dd871957
demaime <aimedemian@gmail.com>
2026-02-16 08:54:07 -0300

    cambio inperceptible para test push

--- src/app/encuestar/page.jsx
+++ src/app/encuestar/page.jsx
@@ -3,7 +3,7 @@ export default function EncuestarPage() {
     <iframe
       src="https://docs.google.com/forms/d/e/1FAIpQLSerGOWkJiBZ6j8X7B7mu2q5gwvS5oBZjGPpn5_nmapCEVsNnA/viewform?embedded=true"
       width="640"
-      height="1514"
+      height="1515"
       frameBorder="0"
       marginHeight="0"
       marginWidth="0"
